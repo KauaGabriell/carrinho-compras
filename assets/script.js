52a17a3372310fad70ec7494883d0e6ba5d76579
Kauã Gabriell <kauagabriell64@gmail.com>
2025-08-18 11:00:30 -0300

    :sparkles: Começando estrutura da lógica dos produtos.

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,42 @@
+const produtosDaLoja = [
+    {
+      id: 1,
+      nome: 'Nike Dunk Low Retro',
+      categoria: 'Casual',
+      preco: 799.99,
+      imagemUrl: './imagens/tenis01.jpg' // Placeholder
+    },
+    {
+      id: 2,
+      nome: 'Air Jordan 1 High',
+      categoria: 'Basquete',
+      preco: 1299.99,
+      imagemUrl: './imagens/tenis01.jpg' // Placeholder
+    },
+    {
+      id: 3,
+      nome: 'Adidas Forum Low',
+      categoria: 'Casual',
+      preco: 599.99,
+      imagemUrl: './imagens/tenis01.jpg' // Placeholder
+    },
+    {
+      id: 4,
+      nome: 'New Balance 550',
+      categoria: 'Casual',
+      preco: 849.99,
+      imagemUrl: './imagens/tenis01.jpg' // Placeholder
+    },
+    {
+      id: 5,
+      nome: 'Nike Air Force 1',
+      categoria: 'Clássicos',
+      preco: 699.99,
+      imagemUrl: './imagens/tenis01.jpg' // Placeholder
+    }
+  ];
+  
+  // Nosso carrinho começa vazio
+  let itensNoCarrinho = [];
 
+  
